c71332ebf2a8023306ab70ab66ca5df35968d2ac
SergioC242 <scroche@ucm.es>
2024-10-25 18:25:11 +0200

    4 Cartas (patatas?)
    en la escena menu

--- HWM/src/classes/BStart.js
+++ HWM/src/classes/BStart.js
@@ -2,8 +2,7 @@ export default class Bstart extends Phaser.GameObjects.Image {
     constructor(scene, x, y){
         super(scene, x, y, 'BStart');
         scene.add.existing(this);
-
-        this.vel = 0; //velocidad
+        
     }
 
     preUpdate(){
--- HWM/src/scenes/CardScene.js
+++ HWM/src/scenes/CardScene.js
@@ -1,12 +1,53 @@
-
+import Bstart from "../classes/BStart.js";
+import Patatas from "../classes/Patatas.js";
 
 export default class CardScene extends Phaser.Scene{
     constructor(){
         super({key: "card"});
     }
 
+    init(){
+        
+    }
 
+    preload(){
+        this.load.image("BStart", "/assets/images/BStart.png")
+        this.load.image("Carta1", "./assets/images/patatas.jpg")
+        this.load.image("Carta2", "./assets/images/patatas.jpg")
+        this.load.image("Carta3", "./assets/images/patatas.jpg")
+        this.load.image("Carta4", "./assets/images/patatas.jpg")
+    }
 
+    create(){
+        //this.add.image(100, 50, "BStart").setOrigin(0, 0).setScale(0.5, 0.5)
+        /** Lo mismo que la línea anterior con clases */
+        let BStart = new Bstart(this, 400, 500);
+        BStart.setInteractive()
+        BStart.on('pointerdown', () => this.StartGame() );
+        BStart.setOrigin(0.4,0.4);
+        BStart.setScale(0.2,0.2);
+
+        
+        /** */
+
+        //this.add.image(400, 450, "patatas").setOrigin(0.5, 0.5).setScale(0.1, 0.1)
+        /** Lo mismo que la línea anterior con clases */
+        let Carta1 = new Patatas(this, 100, 100);
+        Carta1.setOrigin(0.5, 0.5);
+        Carta1.setScale(0.1, 0.1);
+
+        let Carta2 = new Patatas(this, 700, 100);
+        Carta2.setOrigin(0.5, 0.5);
+        Carta2.setScale(0.1, 0.1);
+
+        let Carta3 = new Patatas(this, 100, 450);
+        Carta3.setOrigin(0.5, 0.5);
+        Carta3.setScale(0.1, 0.1);
+
+        let Carta4 = new Patatas(this, 700, 450);
+        Carta4.setOrigin(0.5, 0.5);
+        Carta4.setScale(0.1, 0.1);
+    }
 
     
 }
