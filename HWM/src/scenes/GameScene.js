d411dea32b5feb5f2a72607a1afeb0caae1b1160
SergioC242 <Scroche@ucm.es>
2024-11-01 18:49:53 +0100

    cambio para test error

--- HWM/src/scenes/GameScene.js
+++ HWM/src/scenes/GameScene.js
@@ -9,14 +9,14 @@ export default class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image("background", "/assets/images/space.png");
-        this.load.image("tiles", "assets/tiles/FireSet.png");
-        this.load.tilemapTiledJSON('tilemap', 'assets/tilemap/DemoTilemap.json')
+        this.load.image("tiles", "/assets/tiles/FireSet.png");
+        this.load.tilemapTiledJSON('tilemap', '/assets/tilemap/DemoTilemap.json')
     }
 
     create() {
         this.add.image(400, 250, 'background')
         const map = this.make.tilemap('tilemap');
-        //const tileset = map.addTilesetImage('tiles');  //error?
+        //const tileset = map.addTilesetImage('FireSet', 'tiles');  //error?
 
         //map.createLayer('Todo', tileset)      //mas error?
     }
