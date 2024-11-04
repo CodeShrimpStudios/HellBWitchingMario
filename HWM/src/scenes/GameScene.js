272fc8bf7b7084705c19765af04358dd089e4ab3
SergioC242 <scroche@ucm.es>
2024-11-04 15:53:12 +0100

    Tilemap Rehecho (falta limpiar codigo)

--- HWM/src/scenes/GameScene.js
+++ HWM/src/scenes/GameScene.js
@@ -10,15 +10,33 @@ export default class GameScene extends Phaser.Scene {
     preload() {
         this.load.image("background", "/assets/images/space.png");
         this.load.image("tiles", "/assets/tiles/FireSet.png");
-        this.load.tilemapTiledJSON('tilemap', '/assets/tilemap/DemoTilemap.json')
+        this.load.tilemapTiledJSON('tilemap', 'assets/tilemap/DemoTilemap.json');
+        
     }
 
     create() {
-        this.add.image(400, 250, 'background')
-        const map = this.make.tilemap('tilemap');
+        //this.add.image(400, 250, 'background')
+        //const map = this.make.tilemap({ key: 'tilemap' });
+        //const map = this.make.tilemap('tilemap');
+        //const tiles = map.addTilesetImage('FireSet', 'tiles');
         //const tileset = map.addTilesetImage('FireSet', 'tiles');  //error?
+        //const layer = map.createLayer(0, tiles, 0, 0);
+        //map.createLayer('Capa', tileset)      //mas error?
 
-        //map.createLayer('Todo', tileset)      //mas error?
+
+        this.map = this.make.tilemap({ 
+            key: 'tilemap', 
+            tileWidth: 16, 
+            tileHeight: 16 
+          });
+
+          const tileset1 = this.map.addTilesetImage('FireSet 2', 'tiles');
+
+          // funciona con y sin array
+this.groundLayer = 
+this.map.createLayer('Ground'
+                           , tileset1)
+
     }
 
 }
