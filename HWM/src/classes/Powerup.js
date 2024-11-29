30fa492c45b0e4cdd6fd57fac243a7afb5d9e818
SergioC242 <scroche@ucm.es>
2024-11-29 17:57:45 +0100

    power up sprite

--- HWM/src/classes/Powerup.js
+++ HWM/src/classes/Powerup.js
@@ -1,6 +1,7 @@
 export default class Powerup extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y) {
-        super(scene, x, y);
+    constructor(scene, x, y, texture, frame){
+        super(scene, x, y, texture, frame);
+        
 
         // Añadir a la escena
         scene.add.existing(this);
