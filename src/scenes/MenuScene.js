cb69be7524ef444a6538de4fb60e09f806d7a4a3
SergioC242 <scroche@ucm.es>
2024-12-04 18:37:46 +0100

    portraits metidos

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -16,11 +16,27 @@ export default class MenuScene extends Phaser.Scene {
     preload() {
         this.load.image("BStart", "/assets/images/BStart.png");
         this.load.image("background", "/assets/images/space.png");
+        this.load.image("portrait", "/assets/images/Portrait_Border.png");
+        this.load.spritesheet("pMario", "/assets/images/Portrait_Mario.png", { frameHeight: 32, frameWidth: 18}) ;
+        this.load.image("pYennefer", "/assets/images/Yennefer_SplashArt.png")
     }
 
     create() {
 
         this.add.image(400, 250, 'background');
+
+        let pmario = this.add.sprite(150, 290, 'pMario', 0);
+        pmario.setScale(10)
+
+        let portrait = this.add.image(150, 250, 'portrait');
+        portrait.setScale(4)
+
+        let pyen = this.add.image(650, 350, 'pYennefer');
+        pyen.setScale(4)
+
+        let portrait2 = this.add.image(650, 250, 'portrait');
+        portrait2.setScale(4)
+        
 
         let BStart = new Bstart(this, 400, 250);
         BStart.on('pointerdown', () => this.CardSelect());
